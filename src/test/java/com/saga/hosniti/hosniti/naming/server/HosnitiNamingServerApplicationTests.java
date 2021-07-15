34d0c066ad546416bd0a2e532908318428c632bb
GopinathVaddepally <gopinath.vaddepally@gmail.com>
2021-07-16 00:09:01 +0530

    first commit

---
+++ src/test/java/com/saga/hosniti/hosniti/naming/server/HosnitiNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.hosniti.hosniti.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosnitiNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
